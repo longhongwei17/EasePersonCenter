afe1dc61d34d778960ec5d44c591cb0356049f34
龙伟 <longhongwei17@163.com>
2016-03-25 14:09:15 +0800

    Initial Commit

---
+++ EasePersonCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasePersonCenter
+//
+//  Created by appleDeveloper on 16/3/25.
+//  Copyright © 2016年 appleDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
